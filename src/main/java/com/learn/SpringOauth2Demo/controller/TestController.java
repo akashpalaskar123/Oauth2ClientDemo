bff77af4d203b1e8e758793faafd643c9c5ec08a
palaskara749@gmail.com <you@example.com>
2024-09-02 10:54:57 +0530

    using oauth2.client for login with google and git-hub

---
+++ src/main/java/com/learn/SpringOauth2Demo/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.learn.SpringOauth2Demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/home")
+    public String home (){
+        return "welcome to oauth testing";
+    }
+}
---
+++ src/test/java/com/learn/SpringOauth2Demo/SpringOauth2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.SpringOauth2Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
